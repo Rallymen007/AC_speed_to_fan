434a8a25bbc987b94247f3168f717a3b087a4ff3
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-07-02 11:37:17 +0200

    Initial commit

---
+++ SerialIO/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SerialIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(57, 55);
+            this.trackBar1.Maximum = 260;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(419, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "AC SHM VALUE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 238);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SerialIO/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO.MemoryMappedFiles;
+
+namespace SerialIO
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort port;
+        private MemoryMappedFile shm;
+        private MemoryMappedViewAccessor accessor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            port = new SerialPort("COM5", 9600);
+            port.Open();
+            port.Write("0");
+            /*shm = MemoryMappedFile.OpenExisting("acpmf_physics");
+            accessor = shm.CreateViewAccessor(0, 116);
+
+            new Thread(new ThreadStart(refreshValue)).Start();*/
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int w = trackBar1.Value;
+            //byte[] b = BitConverter.GetBytes(w);
+            //Array.Reverse(b);
+            port.Write(w.ToString());
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            port.Close();
+        }
+
+        private void refreshValue()
+        {
+            try
+            {
+                while (true)
+                {
+                    float w = accessor.ReadSingle(28);
+                    label2.Invoke((MethodInvoker)(() => label2.Text = w.ToString()));
+                    w = (w > 255) ? 255 : w;
+                    port.Write(BitConverter.GetBytes((Int32)Math.Round(w)), 0, 1);
+                    Thread.Sleep(16);
+                }
+            }
+            catch { }
+        }
+    }
+}
